7ca65d5852da6d9ea5ca6df39c0fe60f3c8a3a8d
Dmytrii Nagirniak <dnagir@gmail.com>
2012-03-14 10:45:59 +1100

    rails new rails322-content-type

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rails322ContentType::Application.config.secret_token = '922c8c7a9474e2098f69a78854dca1133c8e4ef6c1ea5ab17256d03a8eef7596b67a945b9dbfb9420e82cad2ebeb2a0e34a1a7f377d70b3cd714988a58ca4c54'
